9553d0df3bd2ec3b55026e7bf8a507169bca7320
big <big_sethawit@hotmail.com>
2019-10-25 00:06:24 +0700

    Rearrange file

---
+++ app/src/main/java/com/u/android_uhome/scene/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.u.android_uhome.scene.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
